316a8906181ba58b4b338cea4d636c02210fa469
aryangtm <100340841+aryangtm@users.noreply.github.com>
2022-03-03 13:41:06 +0530

    Update no_of_correct_and_wrong_ques.py

--- no_of_correct_and_wrong_ques.py
+++ no_of_correct_and_wrong_ques.py
@@ -1,6 +1,6 @@
 ng=int(input("Number you got :"))
 q=int(input("Number of ques :"))
-x=(ng+q*2)/2#x is number of correct ques 
+x=(ng+q*2)/8#x is number of correct ques 
 y=q-x
 print("Number of correct ques=",x)
 print("numbe of wrong ques =",y)
