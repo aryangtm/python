29517dffbdd9004519a7bb1f77441a6c696ae2db
aryangtm <100340841+aryangtm@users.noreply.github.com>
2022-03-03 22:09:51 +0530

    Create greater_in_three.py

---
+++ greater_in_three.py
@@ -0,0 +1,7 @@
+a,b,c=[int(i) for i in input("Enter the value of a ,b and c:").split()]
+if (a>b and a>c):
+    print("a is greater")
+elif(b>a and b>c):
+    print("b is greater ")
+else:
+    print("c is greater")
